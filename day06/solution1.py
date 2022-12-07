cf8cde0605517d572a0b008f60b9a513a391281b
David <steemandavid@gmail.com>
2022-12-07 10:03:51 +0100

    day 6 finished

--- day06/solution1.py
+++ day06/solution1.py
@@ -1,4 +1,16 @@
 import numpy as np
 
 with open('day06/input1', 'r') as file_handle:
-    lines = file_handle.readlines()
+    line = file_handle.read().strip()
+    
+quad = line[:4]
+i = 4
+while i < len(line):
+    if len(set(quad)) == 4:
+        break
+    else:
+        quad = quad[1:] + line[i]
+        i += 1
+        
+print(i)
+    
--- day06/solution2.py
+++ day06/solution2.py
@@ -1,4 +1,16 @@
 import numpy as np
 
 with open('day06/input2', 'r') as file_handle:
-    lines = file_handle.readlines()
+    line = file_handle.read().strip()
+    
+quad = line[:14]
+i = 14
+while i < len(line):
+    if len(set(quad)) == 14:
+        break
+    else:
+        quad = quad[1:] + line[i]
+        i += 1
+        
+print(i)
+    
